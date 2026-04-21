e2274edc7b3f1b485b62c35e0bd2b177762071a1
tejas615 <tejas28032005@gmail.com>
2026-04-22 01:06:29 +0530

    Trained on mnist

---
+++ utils/data.py
@@ -0,0 +1,17 @@
+# utils/data.py
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_mnist_loaders(batch_size=128):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    
+    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST('./data', train=False, transform=transform)
+    
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    return train_loader, test_loader
---
+++ utils/train.py
@@ -0,0 +1,49 @@
+# utils/train.py
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+def train_epoch(model, train_loader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    pbar = tqdm(train_loader, desc='Training')
+    for images, labels in pbar:
+        images, labels = images.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        
+        total_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+        
+        pbar.set_postfix({'loss': total_loss/len(train_loader), 
+                         'acc': 100.*correct/total})
+    
+    return total_loss/len(train_loader), 100.*correct/total
+
+def evaluate(model, test_loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            
+            total_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+    
+    return total_loss/len(test_loader), 100.*correct/total
